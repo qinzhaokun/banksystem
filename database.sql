27ccac0d52a07033d93a58af2ece35092bfdc731
Zhaokun Qin <qinzhaokun@qq.com>
2017-11-03 15:46:27 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,8 @@ use test;
 
 create table account (id int primary key auto_increment not null, account_info_id int, money int);
 
+create index account_id_index on account (account_info_id);
+
 create table account_info (id int primary key auto_increment, account_id int, aname varchar(50), password varchar(50), bank_id int, status int);
 
 create table bank_info (id int primary key auto_increment, bank_name varchar(50), location varchar(50));
